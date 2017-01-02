2b94f7022ac59a20791330ab282b0c9e78f46c7d
Jaimyn Mayer <admin@jabelone.com>
2017-01-03 02:42:03 +1000

    b

--- busbot/handler.js
+++ busbot/handler.js
@@ -4,13 +4,10 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: 'Go Serverless v1.0! Busbot is being executed!',
+      input: event
+    })
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+}
